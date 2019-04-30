593fd440f6e7d7f36326e5001bfa40d9b12c89b1
Rosa <Rosa@RosadeMacBook-Pro.local>
2019-04-29 21:30:27 -0600

    change nlpproject.py. add validation, 80 20 split

--- nlpporject/NLPproject.py
+++ nlpporject/NLPproject.py
@@ -6,12 +6,13 @@ import shutil
 import random
 import numpy as np
 from sklearn.model_selection import train_test_split
+import numpy
 
 
 chinToken = thulac.thulac(seg_only=True, T2S=True, filt=True, deli='_')
-chinToken.cut_f("UN.en-zh.zh", 'tokenizedOriginal')
+chinToken.cut_f("UN.en-zh.zh", 'tokenizedOriginal.txt')
 def getChinese():
-	file = open('tokenizedOriginal', "r", encoding="utf8")
+	file = open('tokenizedOriginal.txt', "r", encoding="utf8")
 	context = file.read()
 	#for line in context:
 	line = context.split('\n')
@@ -28,26 +29,46 @@ def getChinese():
 	return line2
 a = getChinese()
 # print(a)
-nopunc = open('nopuncoriginal', 'w', encoding="utf8")
+nopunc = open('nopuncoriginal.txt', 'w', encoding="utf8")
 for i in range(len(a)):
 	nopunc.write(a[i])
 	nopunc.write('\n')
 nopunc.close()
 
 
-#80 20 split
-with open("nopuncoriginal", "r") as f:
-	data = f.read().split('\n')
-	# data = f.read()
-	X_test, X_train = train_test_split(data, test_size=0.80, train_size=0.20, shuffle=True) #random_state=101
-	f.close()
-print(X_test)
-print(X_train)
-smallTest = open('traindataOriginal', 'w', encoding="utf8")
-for i in range(len(X_test)):
-	smallTest.write(X_test[i])
-	smallTest.write('\n')
-smallTrain = open('testdataOriginal', 'w', encoding="utf8")
-for i in range(len(X_train)):
-	smallTrain.write(X_train[i])
-	smallTrain.write('\n')
+
+
+
+#validation set
+with open('nopuncoriginal.txt','r') as d:
+	temp1 = d.read().split('\n')
+with open('validation.txt', 'w') as e:
+	num = np.random.choice(len(temp1), 5000)
+	for i in range(len(num)):
+		e.write(temp1[i])
+		e.write('\n')
+
+
+#delete sentences in validation
+copytemp = temp1
+with open('validation.txt','r') as f:
+	temp2 = f.read().split('\n')
+for i in temp2:
+	if i in copytemp:
+		copytemp.remove(i)
+
+#80 train 20 test split
+testx, trainx = train_test_split(copytemp, test_size=0.20, train_size=0.80, shuffle=True) #random_state=101
+test = open('traindataOriginal.txt', 'w', encoding="utf8")
+for i in range(len(testx)):
+	test.write(testx[i])
+	test.write('\n')
+train = open('testdataOriginal.txt', 'w', encoding="utf8")
+for i in range(len(trainx)):
+	train.write(trainx[i])
+	train.write('\n')
+
+
+
+
+
